b1f95ca129d58fe0f069793b23e22252b776f352
Matheus7k <06matheus.oliveira@gmail.com>
2023-03-15 15:46:04 -0300

    Criado verificações

--- battleNaval/Program.cs
+++ battleNaval/Program.cs
@@ -23,13 +23,12 @@ internal class Program
         //Console.WriteLine(VerifyRowColumn(board, 0, 4, carrier, "horizontal", aux));
         //Console.WriteLine(VerifyRowColumn(board, 0, 19, carrier, "horizontal", aux));
 
-        Console.WriteLine(VerifyRowToRight(board, 0, 4, carrier, "horizontal", aux));
+        Console.WriteLine(VerifyRowToRight(board, 0, 0, carrier, "horizontal", aux));
 
         if (VerifyRowToRight(board, 0, 2, carrier, "horizontal", aux) == 0)
         {
-            Console.WriteLine(VerifyRowToLeft(board, 0, 2, carrier, "horizontal", aux));
+            Console.WriteLine(VerifyRowToLeft(board, 0, 0, carrier, "horizontal", aux));
         }
-
     }
 
     static public int VerifyRowToRight(Board board, int row, int column, Ship ship, string orientation, int[,] aux)
@@ -43,7 +42,7 @@ internal class Program
                 // Percorre as colunas;
                 for (int coluna = column; coluna < column + ship._life - 1; coluna++)
                 {
-                    // Verifica em cima e em baixo;
+                    // Verifica em cima, em baixo e uma posição a frente;
                     if (row == 0)
                     {
                         if (aux[row + 1, coluna] != 0 || aux[row, coluna + 1] != 0)
@@ -188,15 +187,74 @@ internal class Program
 
     static public int VerifyColumnToDown(Board board, int row, int column, Ship ship, string orientation, int[,] aux)
     {
+        // Verifica se cabe na vertical
         if (orientation == "vertical")
         {
+            // Verifica se cabe na vertical para baixo
             if (row + ship._life - 1 <= board._board.GetLength(0) - 1)
             {
+                // Percorre as linhas
+                for(int linha = row; linha < row + ship._life - 1; linha++)
+                {
+                    // Verifica em cima e em baixo e uma posição a frente
+                    if(column == 0)
+                    {
+                        if (aux[linha,column + 1] != 0 || aux[linha + 1, column] != 0)
+                        {
+                            return 0;
+                        }
+                    }
+                    else if(column == 19)
+                    {
+                        if (aux[linha, column - 1] != 0 || aux[linha + 1, column] != 0)
+                        {
+                            return 0;
+                        }
+                    }
+                    else
+                    {
+                        if (aux[linha, column - 1] != 0 || aux[linha, column + 1] != 0 || aux[linha + 1,column] != 0)
+                        {
+                            return 0;
+                        }
+                    }
+                }
+
+                // Verifica diagonal frente e tras
+                if (column == 0)
+                {
+                    if (aux[row + ship._life, column] != 0 || aux[row + ship._life, column + 1] != 0) 
+                    {
+                        return 0;
+                    }
+                    else if(aux[row - 1, column] != 0 || aux[row - 1, column + 1] != 0){
+                        return 0;
+                    }
+                }
+                else if (column == 19)
+                {
+                    if (aux[row + ship._life, column] != 0 ||  aux[row + ship._life, column - 1] != 0)
+                    {
+                        return 0;
+                    }
+                    else if (aux[row - 1, column] != 0 || aux[row - 1, column - 1] != 0) 
+                    {
+                        return 0;
+                    }
+                }
+                else
+                {
+                    if(aux[row + ship._life, column] != 0 || aux[row + ship._life, column + 1] != 0 ||  aux[row + ship._life, column - 1] != 0) 
+                    {
+                        return 0;
+                    }
+                    else if (aux[row - 1, column] != 0 || aux[row - 1, column - 1] != 0 || aux[row - 1, column + 1] != 0) 
+                    {
+                        return 0;
+                    }
+                }
+
                 return 1;
-            }
-            else if (row - (ship._life - 1) >= 0)
-            {
-                return 2;
             }
 
             return 0;
@@ -205,8 +263,79 @@ internal class Program
         return 0;
     }
 
-    static public int VerifyCOlunmToUp(Board board, int row, int column, Ship ship, string orientation, int[,] aux)
+    static public int VerifyColumnToUp(Board board, int row, int column, Ship ship, string orientation, int[,] aux)
     {
+        if (orientation == "vertical") 
+        {
+            // Verifica se cabe na vertical de baixo para cima
+            if (row - (ship._life - 1) >= 0)
+            {
+                // Percorre a linha
+                for (int linha = row; linha > row - (ship._life - 1); linha--)
+                {
+                    // verifica de uma lado e de outro
+                    if(column == 0)
+                    {
+                        if (aux[linha, column + 1] != 0 || aux[linha - 1, column] != 0) 
+                        {
+                            return 0;
+                        }
+                    }
+                    else if (column == 19) 
+                    {
+                        if (aux[linha, column - 1] != 0 || aux[linha - 1, column] != 0) 
+                        {
+                            return 0;
+                        }
+                    }
+                    else 
+                    {
+                        if(aux[linha, column + 1] != 0 || aux[linha - 1, column] != 0 || aux[linha, column - 1] != 0) 
+                        {
+                            return 0;
+                        }
+                    }
+                }
+
+                // Verifica diagonal frente e tras
+                if (column == 0) 
+                {
+                    if(aux[row - ship._life, column] != 0 || aux[row - ship._life, column + 1] != 0) 
+                    {
+                        return 0;
+                    }
+                    else if(aux[row - 1, column] != 0 || aux[row - 1, column + 1] != 0) 
+                    {
+                        return 0;
+                    }
+                }
+                else if (column == 19) 
+                {
+                    if(aux[row - ship._life, column] != 0 || aux[row - ship._life, column - 1] != 0) 
+                    {
+                        return 0;
+                    }
+                    else if(aux[row - 1, column] != 0 || aux[row - 1, column - 1] != 0) 
+                    {
+                        return 0;
+                    }
+                }
+                else 
+                {
+                    if (aux[row - ship._life, column] != 0 || aux[row - ship._life, column + 1] != 0 || aux[row - ship._life, column - 1] != 0) 
+                    {
+                        return 0;
+                    }
+                    else if (aux[row - 1, column] != 0 || aux[row - 1, column + 1] != 0 || aux[row - 1, column - 1] != 0) 
+                    {
+                        return 0;
+                    }
+                }
+                
+                return 2;
+            }
+        }
+
         return 0;
     }
 }
